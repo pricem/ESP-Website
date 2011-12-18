9156db2265bfe17ef9ea8f397b771baf15b5ff4a
Michael Price <price@kilentra.net>
2011-12-18 18:58:11 -0500

    Wrap esp_user setup Javascript code in a function so it can be repeated

--- esp/public/media/scripts/content/user_data.js
+++ esp/public/media/scripts/content/user_data.js
@@ -10,36 +10,40 @@ if (typeof(window["$j"]) == "undefined") {
 }
 
 var esp_user = {};
-var esp_user_keys = new Array('cur_username','cur_email','cur_first_name','cur_last_name','cur_other_user','cur_retTitle', 'cur_admin','cur_grade','cur_roles','cur_qsd_bits');
 
-for (var i=0; i < esp_user_keys.length; i++) {
-    var tmp = $j.cookie(esp_user_keys[i]);
-    if (tmp) {
-	esp_user[esp_user_keys[i]] = tmp;
+function load_cookies()
+{
+    var esp_user_keys = new Array('cur_username','cur_email','cur_first_name','cur_last_name','cur_other_user','cur_retTitle', 'cur_admin','cur_grade','cur_roles','cur_qsd_bits');
+
+    for (var i=0; i < esp_user_keys.length; i++) {
+        var tmp = $j.cookie(esp_user_keys[i]);
+        if (tmp) {
+        esp_user[esp_user_keys[i]] = tmp;
+        }
+        /* These cookies are escape for potential unicode.
+         * : see esp/middleware/esperrormiddleware.py
+         */
     }
-    /* These cookies are escape for potential unicode.
-     * : see esp/middleware/esperrormiddleware.py
-     */
-}
-esp_user['cur_email'] = unescape(esp_user['cur_email']);
-esp_user['cur_first_name'] = unescape(esp_user['cur_first_name']);
-esp_user['cur_last_name'] = unescape(esp_user['cur_last_name']);
-esp_user['cur_grade'] = parseInt(esp_user['cur_grade']);
-if (esp_user['cur_roles']) {
-    esp_user['cur_roles'] = unescape(esp_user['cur_roles']).split(',');
-} else {
-    esp_user['cur_roles'] = [];
-}
-
-var esp_user_login = null;
-
-if (esp_user['cur_username']) {
-    esp_user_login = '<table border="0" cellpadding="0" cellspacing="0" summary=" "><tr><td width="100%"><div class="divformcol1">&nbsp;</div></td><td style="text-align:left"><div class="divformcol1" style="text-align:left">';
-
-    if (esp_user['cur_other_user'] == '1') {
-	esp_user_login += '<a href="/myesp/switchback/">Go back to ' + esp_user['cur_retTitle'] + '</a>';
+    esp_user['cur_email'] = unescape(esp_user['cur_email']);
+    esp_user['cur_first_name'] = unescape(esp_user['cur_first_name']);
+    esp_user['cur_last_name'] = unescape(esp_user['cur_last_name']);
+    esp_user['cur_grade'] = parseInt(esp_user['cur_grade']);
+    if (esp_user['cur_roles']) {
+        esp_user['cur_roles'] = unescape(esp_user['cur_roles']).split(',');
     } else {
-	esp_user_login += 'Welcome, ' + esp_user['cur_first_name'] + '!';
+        esp_user['cur_roles'] = [];
     }
-    esp_user_login += '</div></td><td colspan="2"><div class="divformcol2" style="text-align:right"><a href="/myesp/signout/">Sign Out</a></div></tr></table>';
+
+    var esp_user_login = null;
+
+    if (esp_user['cur_username']) {
+        esp_user_login = '<table border="0" cellpadding="0" cellspacing="0" summary=" "><tr><td width="100%"><div class="divformcol1">&nbsp;</div></td><td style="text-align:left"><div class="divformcol1" style="text-align:left">';
+
+        if (esp_user['cur_other_user'] == '1') {
+        esp_user_login += '<a href="/myesp/switchback/">Go back to ' + esp_user['cur_retTitle'] + '</a>';
+        } else {
+        esp_user_login += 'Welcome, ' + esp_user['cur_first_name'] + '!';
+        }
+        esp_user_login += '</div></td><td colspan="2"><div class="divformcol2" style="text-align:right"><a href="/myesp/signout/">Sign Out</a></div></tr></table>';
+    }
 }
